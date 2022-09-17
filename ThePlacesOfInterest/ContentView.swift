978f85e52ee1b520aa9cbf81f567132e56c0ea2b
monkeyhammercopiseded <Wf6350177@163.com>
2022-09-17 08:26:40 +0800

    Initial Commit

---
+++ ThePlacesOfInterest/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ThePlacesOfInterest
+//
+//  Created by Monkey hammer on 2022/9/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ThePlacesOfInterest/ThePlacesOfInterestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThePlacesOfInterestApp.swift
+//  ThePlacesOfInterest
+//
+//  Created by Monkey hammer on 2022/9/17.
+//
+
+import SwiftUI
+
+@main
+struct ThePlacesOfInterestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
